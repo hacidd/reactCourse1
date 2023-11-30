461384c2ce7f8e29deeced881bc7ca1c5af5daa4
hacidd <radoslawef@gmail.com>
2023-11-30 13:23:59 +0100

    added tabButton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 
 import { CORE_CONCEPTS } from './data';
-import Header from './components/Header.jsx'
+import CoreConcept from './components/CoreConcept.jsx';
 
+import Header from './components/Header/Header.jsx'
+import TabButton from './components/TabButton.jsx';
+<link rel="stylesheet" href="" />
 // function CoreConcept(props) {
 // 	return (
 // 	<li>
@@ -36,7 +39,16 @@ function App() {
 				</ul>
 
 				</section>
-				
+				<section id='examples'>
+					<h2>Examples</h2>
+					<menu>
+						<TabButton>Components</TabButton>
+						<TabButton>JSX</TabButton>
+						<TabButton>Props</TabButton>
+						<TabButton>State</TabButton>
+					</menu>
+
+				</section>
 			</main>
 		</div>
 	);
